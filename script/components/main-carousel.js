c159e6d44a63b694e3386b1cb7eee0bbd468308d
LeandroJSilva1989 <lelejs@hotmaill.com>
2021-10-12 20:44:29 -0300

    cria o js do main-carousel

--- script/components/main-carousel.js
+++ script/components/main-carousel.js
@@ -1,1 +1,17 @@
-alert("Caixa Troll");
+// 1 acessar a Janela (browser)
+// 2 Pegar o HTML todo
+// 3 Pegar o botão
+// 4 Saber que o botão está sendo clicado
+
+// 5 Acessar a Janela 
+// 6 Pegar o HTML todo
+// 7 Pegar o elements
+// 8 Mover o elements para direita
+ 
+ const btnRight = window.document.querySelector(".button-arrow.-right");
+ const elements = window.document.querySelector(".elements");
+
+ btnRight.addEventListener("click", function( ) {
+
+ });
+
