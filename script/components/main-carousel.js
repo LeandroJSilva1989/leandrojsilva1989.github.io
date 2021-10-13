09f03e2c9f8b247b5e85f88e7e9d540bea40fc7a
LeandroJSilva1989 <lelejs@hotmaill.com>
2021-10-12 21:17:24 -0300

    cria o js do main-carousel

--- script/components/main-carousel.js
+++ script/components/main-carousel.js
@@ -21,7 +21,7 @@
  });
 
  btnLeft.addEventListener("click", function() {
-    pixels =- 100;
-    elements.style = `transform: translateX(${pixels}px)`;
- });
+         pixels = -100;
+         elements.style = `transform: translateX(${pixels}px)`;
+     });
 
