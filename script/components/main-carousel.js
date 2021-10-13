442c22ee467111b554840b3d02b209fbc92c3098
LeandroJSilva1989 <lelejs@hotmaill.com>
2021-10-12 21:18:05 -0300

    cria o js do main-carousel

--- script/components/main-carousel.js
+++ script/components/main-carousel.js
@@ -16,12 +16,12 @@
  let pixels = 100;
 
  btnRight.addEventListener("click", function() {
-    pixels =+ 100;
+    pixels = pixeels + 100;
     elements.style = `transform: translateX(${pixels}px)`;
  });
 
  btnLeft.addEventListener("click", function() {
-         pixels = -100;
+         pixels = pixels - 100;
          elements.style = `transform: translateX(${pixels}px)`;
      });
 
